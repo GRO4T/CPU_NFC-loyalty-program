ff40231a67d7bd270119ca390ef848c0de480d07
Damian Kolaska <damian.kolaska99@gmail.com>
2023-12-09 15:11:12 +0100

    fix: make payment_card_serial a primary key

--- backend/nfclp/loyalty/models.py
+++ backend/nfclp/loyalty/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class Account(models.Model):
-    payment_card_serial = models.CharField(max_length=255)
+    payment_card_serial = models.CharField(max_length=255, primary_key=True)
     points = models.IntegerField()
