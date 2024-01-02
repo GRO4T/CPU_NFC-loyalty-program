b0a031607ecc279c84092e6a8d7923af9b4af6aa
Damian Kolaska <damian.kolaska99@gmail.com>
2024-01-02 20:11:52 +0100

    feat: add sign in and register account buttons

--- mobile_app/app/src/main/java/com/example/nfcloyaltyprogrammobileapp/screen/MainScreen.kt
+++ mobile_app/app/src/main/java/com/example/nfcloyaltyprogrammobileapp/screen/MainScreen.kt
@@ -1,14 +1,24 @@
 package com.example.nfcloyaltyprogrammobileapp.screen
 
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.nfcloyaltyprogrammobileapp.ui.theme.NFCLoyaltyProgramMobileAppTheme
 import com.example.nfcloyaltyprogrammobileapp.viewmodel.MainViewModel
+
+const val TAG = "MainActivity";
 
 @Composable
 internal fun MainRoute() {
@@ -20,9 +30,17 @@ internal fun MainRoute() {
 internal fun MainScreen(viewModel: MainViewModel) {
     NFCLoyaltyProgramMobileAppTheme {
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-            Text(
-                text = "Bring payment card to NFC reader to sign up",
-            )
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(modifier = Modifier.size(300.dp), onClick = {Log.d(TAG, "Sign in")}) {
+                    Text(text = "Sign in", fontSize = 50.sp)
+                }
+                Button(onClick = { Log.d(TAG, "Register account")}) {
+                    Text("Register an account")
+                }
+            }
         }
     }
 }
