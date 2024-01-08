407d059e9ebf42f608e31bd388b0fa4a879069ff
LDTips <batruchs@gmail.com>
2024-01-08 15:34:45 +0100

    Initial API handling for account creation

--- nfc_terminal/app/src/main/java/com/example/nfcloyaltyprogramterminal/MainActivity.kt
+++ nfc_terminal/app/src/main/java/com/example/nfcloyaltyprogramterminal/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.nfcloyaltyprogramterminal
 
+import android.nfc.NfcAdapter
+import android.nfc.Tag
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,11 +11,61 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.example.nfcloyaltyprogramterminal.network.ApiService
 import com.example.nfcloyaltyprogramterminal.ui.theme.NFCLoyaltyProgramTerminalTheme
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : NfcAdapter.ReaderCallback, ComponentActivity() {
+    var tagSerialNumber by mutableStateOf("")
+
+    private val apiService: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://nfclp.onrender.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+    override fun onStart() {
+        super.onStart()
+        NfcAdapter.getDefaultAdapter(this)?.enableReaderMode(
+            this,
+            this,
+            NfcAdapter.FLAG_READER_NFC_A,
+            null
+        )
+    }
+
+    override fun onStop() {
+        super.onStop()
+        NfcAdapter.getDefaultAdapter(this)?.disableReaderMode(this)
+    }
+
+    private fun registerAccount(cardId: String) {
+        lifecycleScope.launch {
+            try {
+                val response = apiService.registerAccount(cardId)
+                Log.d("ApiActivity", "Registered!")
+            } catch (e: Exception) {
+                Log.e("ApiActivity", "Registration Error", e)
+            }
+        }
+    }
+    override fun onTagDiscovered(tag: Tag) {
+        tagSerialNumber = tag.id.joinToString(separator = ":") { byte -> "%02x".format(byte) }
+        Log.d("NfcComponentActivity", "Discovered tag with serial number: $tagSerialNumber")
+
+        registerAccount(tagSerialNumber)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
---
+++ nfc_terminal/app/src/main/java/com/example/nfcloyaltyprogramterminal/network/api.kt
@@ -0,0 +1,11 @@
+package com.example.nfcloyaltyprogramterminal.network
+
+import com.example.nfcloyaltyprogramterminal.data_structs.StatusResponse
+import retrofit2.Response
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface ApiService {
+    @POST("accounts/{cardId}")
+    suspend fun registerAccount(@Path("cardId") cardId: String): Response<StatusResponse>
+}
